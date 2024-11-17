c6b3cfac8780a15bd42dd1038b52b27f300bfc35
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-11-17 08:59:19 -0800

    chore: remove greeter API and related tests

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,13 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-import { greeterRoutes } from "./routes/greeterRoutes.js";
 import { userRoutes } from "./routes/userRoutes.js";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-greeterRoutes(app);
 userRoutes(app);
 
 app.get("/", (req, res) => {
